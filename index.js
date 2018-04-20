1b077508cf00602607f42ba2c22dabe14f800190
Daksh Miglani <hello@dak.sh>
2018-04-20 12:11:56 +0530

    add fetch for node.

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 let price = require('./lib/price')
+
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+  global.fetch = require('node-fetch');
+}
+
 module.exports = price
